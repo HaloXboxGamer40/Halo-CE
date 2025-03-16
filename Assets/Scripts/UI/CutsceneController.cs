4cbf56ab354e284921f34c6f07f1be04b31856e0
ArrowStudiosWasTaken <95988623+ArrowStudiosWasTaken@users.noreply.github.com>
2025-03-16 09:32:45 -0700

    Dump 5 - Updated a bunch of stuff

---
+++ Assets/Scripts/UI/CutsceneController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
+
+public class CutsceneController : MonoBehaviour
+{
+    // Reference to the VideoPlayer component
+    public VideoPlayer videoPlayer;
+
+    // Reference to the GameObject cutsceneCam
+    public GameObject cutsceneCam;
+
+    // Reference to the GameObject map
+    public GameObject map;
+
+    public GameObject player;
+
+    public GameObject skipText;
+
+    public bool skipCutscene;
+
+    void Start()
+    {
+        // Subscribe to the VideoPlayer's loopPointReached event to detect when the video is finished
+        videoPlayer.loopPointReached += OnVideoEnd;
+
+        if (skipCutscene)
+            StartCoroutine(skipTextCode());
+    }
+
+    void Update()
+    {
+        // Check if the space bar is pressed
+        if (Input.GetKeyDown(KeyCode.Space) && skipCutscene)
+        {
+            EndCutscene();
+        }
+    }
+
+    // Event handler for when the video ends
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        EndCutscene();
+    }
+
+    // Method to handle ending the cutscene
+    void EndCutscene()
+    {
+        // Stop the video
+        videoPlayer.Stop();
+
+        // Hide the cutsceneCam GameObject
+        Destroy(cutsceneCam);
+
+        // Show the map GameObject
+        if (skipCutscene) {
+            map.SetActive(true);
+
+            player.SetActive(true);
+        }
+        // Destroy the VideoPlayer GameObject (if it is separate)
+        Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        // Unsubscribe from the event to avoid memory leaks
+        videoPlayer.loopPointReached -= OnVideoEnd;
+    }
+
+    IEnumerator skipTextCode()
+    {
+        yield return new WaitForSeconds(4f);
+        Destroy(skipText);
+    }
+}
